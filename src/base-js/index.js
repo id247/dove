43a52ae62b3276378636291d69e75788395ceebf
simplecss <simplecss@yandex.ru>
2016-08-25 14:33:58 +0300

    init

---
+++ src/base-js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+console.log('run');
+
+$(function(){
+
+});
+
+
+
---
+++ webpack.staging.config.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+var appSettings = path.join(__dirname, 'src/js/settings/settings-staging.js');
+
+module.exports = {
+	devtool: '#inline-source-map',
+	entry: {
+		'base-staging': [
+			'./src/base-js'
+		],
+	},
+	output: {
+		path: path.join(__dirname, '/production/assets/js'),
+		filename: '[name].min.js',
+		publicPath: path.join(__dirname, '/production/assets/js'),
+		pathinfo: true
+	},
+
+	resolve: {
+		modulesDirectories: ['node_modules'],
+		extentions: ['', '.js'],
+		alias: {
+			appSettings: appSettings,
+		}
+	},
+
+	module: {
+		noParse: [
+		],
+		loaders: [
+			{   test: /\.js$/, 
+				loader: 'babel',
+				include: [
+					path.join(__dirname, '/src/js'),
+				], 
+				query: {
+					cacheDirectory: true,
+					presets: ['es2015', 'stage-2']
+				}
+			},
+		]
+	},
+	plugins: [     
+	]
+};
+
