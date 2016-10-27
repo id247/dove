feba859052c6827ad9b76221f8690dbd47cd3a06
simplecss <simplecss@yandex.ru>
2016-10-27 13:45:44 +0300

    links to profiles added

--- src/forum-js/components/forum/PostInfo.js
+++ src/forum-js/components/forum/PostInfo.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { PromoOptions } from 'appSettings';
 
 const PostInfo = (props) => {
 	const date = new Date(props.post.CreatedDate).toLocaleString('ru-RU');
+
+	let name = (
+		<span className="post__name">{props.user.firstName} {props.user.lastName} {(props.isPsyco ? '(Психолог)' : '')}</span>
+	);
+
+	if (document.location.href.indexOf('competition') > -1){
+
+		name = (
+			<a href={PromoOptions.server + '/user/user.aspx?user=' + props.user.id} className="post__name link" target="_blank">{props.user.firstName} {props.user.lastName}</a>
+		);
+	}
+
 	return(
 		<div className="post__info">
-
-			<span className="post__name">{props.user.firstName} {props.user.lastName} {(props.isPsyco ? '(Психолог)' : '')}</span>
+			{name}
 			{' / '}
 			<span className="post__time">{date}</span>
 
